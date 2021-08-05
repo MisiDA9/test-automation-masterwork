273a1c36cff709113af40ee52fd53bb8e4411bb1
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 08:13:59 +0200

    AllTheProducts was added

---
+++ ReiszMihalyMasterwork/src/test/java/Pages/AllTheProducts.java
@@ -0,0 +1,49 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+public class AllTheProducts {
+  WebDriver driver;
+
+  public AllTheProducts(WebDriver driver) {
+    this.driver = driver;
+  }
+
+  public WebElement getFilterSelect() {
+    return driver.findElement(By.xpath("//*[@id=\"js-product-list-top\"]/div[2]/div/div[1]/button/i"));
+  }
+
+  public WebElement getFromLowToHigh() {
+    return driver.findElement(By.xpath("//*[@id=\"js-product-list-top\"]/div[2]/div/div[1]/div/a[5]"));
+  }
+
+  public WebElement getNextButton() {
+    return driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/nav/div[2]/ul/li[3]/a/i"));
+  }
+
+
+  public List<Integer> getPrices() {
+//    List<WebElement> prices = driver.findElements(By.xpath("//span[@class='price']"));
+    List<WebElement> prices = driver.findElements(By.className("price"));
+    ArrayList<Integer> myList = new ArrayList<Integer>();
+
+    for (int i = 0; i < prices.size(); i++) {
+      if (prices.get(i).getText().length() == 5) {
+        myList.add(i, (parseInt(prices.get(i).getText().substring(1, 2))));
+      } else if (prices.get(i).getText().length() == 6) {
+        myList.add(i, (parseInt(prices.get(i).getText().substring(1, 3))));
+      } else if (prices.get(i).getText().length() == 7)
+        myList.add(i, (parseInt(prices.get(i).getText().substring(1, 4))));
+    }
+
+    return myList;
+  }
+
+}
