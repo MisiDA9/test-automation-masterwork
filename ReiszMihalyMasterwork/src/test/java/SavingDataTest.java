62269d055f0e09ac63a4c827e870336b775545fc
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 23:22:38 +0200

    SavingDataTest was modified

--- ReiszMihalyMasterwork/src/test/java/SavingDataTest.java
+++ ReiszMihalyMasterwork/src/test/java/SavingDataTest.java
@@ -5,7 +5,6 @@ import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,14 +13,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SavingDataTest extends BaseTest {
-
   String folderName = "exportedProductDescriptions" + System.currentTimeMillis();
 
   @Test
   @DisplayName("TC13 - Download a list of data to .txt ")
   @Feature("Accessories page")
   @Description("After saving a list of data (accessories's name and prices) from the accessories page into a text file, assert that, the file has the same list, what you can find on the accessories page")
-
   public void savingData() {
     Accessories accessories = new Accessories(driver);
     accessories.open();
@@ -37,7 +34,7 @@ public class SavingDataTest extends BaseTest {
     try {
       Files.createDirectories(path);
     } catch (IOException e) {
-      System.err.println("Something went wrong");
+      System.err.println("Something went wrong, the folder wasn't created");
     }
   }
 
@@ -47,7 +44,7 @@ public class SavingDataTest extends BaseTest {
     try {
       Files.write(Paths.get(folderName + "/accessories.txt"), accessories.getProductDescription());
     } catch (IOException e) {
-      System.err.println("Something went wrong2");
+      System.err.println("Something went wrong, the String wasn't exported to the txt");
     }
   }
 
@@ -57,10 +54,9 @@ public class SavingDataTest extends BaseTest {
     try {
       result = new ArrayList<>(Files.readAllLines(Paths.get(folderName + "/accessories.txt")));
     } catch (IOException e) {
-      System.err.println("File not exist");
+      System.err.println("File doest not exist");
     }
     return result;
   }
-
 }
 
