70bfe2895edec391241ec49a99512b412c60c6f3
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 23:18:41 +0200

    DataModificationTest was modified

--- ReiszMihalyMasterwork/src/test/java/DataModificationTest.java
+++ ReiszMihalyMasterwork/src/test/java/DataModificationTest.java
@@ -1,6 +1,5 @@
 import Pages.Addresses;
 import Pages.NewAddresses;
-import Pages.Registration;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
@@ -10,12 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.time.Duration;
-import java.util.Random;
 
 public class DataModificationTest extends BaseTest {
-
-
   @Test
   @DisplayName("TC10 - ZIP code modification")
   @Feature("Addresses: updating information")
@@ -30,21 +25,14 @@ public class DataModificationTest extends BaseTest {
     Assertions.assertTrue(addresses.getFeedbackMessage().getText().equals("Address successfully updated!"));
     }
 
-    @Step ("Saving a new valid address")
-    public void saveANewAddress() {
-      NewAddresses newAddresses = new NewAddresses(driver);
-      newAddresses.open();
-      newAddresses.newAddressesInput("23 Bunting Road", "Naples", "Florida", "11111");
-    }
-
-    @Step ("Modify the already exist address")
-    public void modifyTheAddress() {
-      NewAddresses newAddresses = new NewAddresses(driver);
-      Addresses addresses = new Addresses(driver);
-      addresses.updateFirst();
-      newAddresses.getPostcode().sendKeys(Keys.DELETE);
-      newAddresses.getPostcode().clear();
-      newAddresses.getPostcode().sendKeys("12345");
-      newAddresses.getSaveButton().click();
-    }
+  @Step ("Modify the already existed address")
+  public void modifyTheAddress() {
+    NewAddresses newAddresses = new NewAddresses(driver);
+    Addresses addresses = new Addresses(driver);
+    addresses.updateFirst();
+    newAddresses.getPostcode().sendKeys(Keys.DELETE);
+    newAddresses.getPostcode().clear();
+    newAddresses.getPostcode().sendKeys("12345");
+    newAddresses.getSaveButton().click();
+  }
 }
