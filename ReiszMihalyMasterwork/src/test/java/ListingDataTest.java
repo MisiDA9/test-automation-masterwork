10052a5e09898cdbee5451bf8d93f5a0fe09aeb9
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 23:19:02 +0200

    ListingDataTest was modified

--- ReiszMihalyMasterwork/src/test/java/ListingDataTest.java
+++ ReiszMihalyMasterwork/src/test/java/ListingDataTest.java
@@ -1,51 +1,36 @@
 import Pages.AllTheProducts;
-import Pages.HomePage;
-import Pages.Login;
-import Pages.MyAccount;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.time.Duration;
 
 public class ListingDataTest extends BaseTest {
-
-
   @Test
   @Feature("Listing data")
   @Description("After listing all products in the webshop and sorting for ascending price orders, every price should be less or equal to the next price.")
   @DisplayName("TC_06 Listing products by price - ascending")
   public void listingData() throws InterruptedException {
-    
-//    Login loginPage = new Login(driver);
-//    loginPage.open();
-//
-//    loginPage.login("michael@test.com", "pass123");
-//
-//
-//    MyAccount myAccount = new MyAccount(driver);
-//    myAccount.getLogo().click();
-//
-//    HomePage homePage = new HomePage(driver);
-//    homePage.allTheProdcuts().click();
-
-
     AllTheProducts allTheProducts = new AllTheProducts(driver);
     allTheProducts.open();
 
-    allTheProducts.getFilterSelect().click();
-    allTheProducts.getFromLowToHigh().click();
+    listingTheProducts();
 
-
-//    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-    wait.until(ExpectedConditions.visibilityOf(allTheProducts.productList()));
+    wait.until(ExpectedConditions.visibilityOf(allTheProducts.getProductList()));
     Thread.sleep(1000);
     for (int i = 0; i < allTheProducts.getPrices().size()-1; i++) {
       Assertions.assertTrue(allTheProducts.getPrices().get(i) <= allTheProducts.getPrices().get(i + 1));
     }
+  }
+
+  @Step("Listing and sorting all the products by price in ascending order")
+  public void listingTheProducts() {
+    AllTheProducts allTheProducts = new AllTheProducts(driver);
+    allTheProducts.getFilterSelect().click();
+    allTheProducts.getFromLowToHigh().click();
   }
 }
 
