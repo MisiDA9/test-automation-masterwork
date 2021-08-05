38d38625cd76784f900b777ae3d6685ad1d534a2
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 23:19:51 +0200

    Accessories was modified

--- ReiszMihalyMasterwork/src/test/java/Pages/Accessories.java
+++ ReiszMihalyMasterwork/src/test/java/Pages/Accessories.java
@@ -3,11 +3,9 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Integer.parseInt;
 
 public class Accessories {
   WebDriver driver;
@@ -21,17 +19,11 @@ public class Accessories {
   }
 
   public List<String> getProductDescription() {
-
     List<WebElement> productsDescriptionWebelements = driver.findElements(By.className("product-description"));
     ArrayList<String> productsDescription = new ArrayList<String>();
-
     for (int i = 0; i < productsDescriptionWebelements.size(); i++) {
       productsDescription.add(i,productsDescriptionWebelements.get(i).getText());
     }
-
     return productsDescription;
   }
-
-
-
 }
