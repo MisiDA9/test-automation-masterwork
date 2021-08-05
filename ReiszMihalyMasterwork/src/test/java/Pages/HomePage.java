c609e1a81e452384da64fe668aea01f8ef22dbcc
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 23:20:46 +0200

    HomePage was modified

--- ReiszMihalyMasterwork/src/test/java/Pages/HomePage.java
+++ ReiszMihalyMasterwork/src/test/java/Pages/HomePage.java
@@ -2,23 +2,16 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 
 public class HomePage {
-
   WebDriver driver;
 
   public HomePage(WebDriver driver) {
     this.driver = driver;
   }
 
-  //username
   public String getActualName() {
     return driver.findElement(By.xpath("//*[@id=\"_desktop_user_info\"]/div/a[2]/span")).getText();
   }
-
-  public WebElement allTheProdcuts () {
-    return driver.findElement(By.xpath("//*[@id=\"content\"]/section/a"));
-  }
-
 }
