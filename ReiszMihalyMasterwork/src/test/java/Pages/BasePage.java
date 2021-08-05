cca19419f9b8d33b49c8c798a50e4a3c3618ea0b
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 15:45:32 +0200

    BasePage was modified

--- ReiszMihalyMasterwork/src/test/java/Pages/BasePage.java
+++ ReiszMihalyMasterwork/src/test/java/Pages/BasePage.java
@@ -1,15 +1,15 @@
-//package Pages;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//
-//public class BasePage {
-//  protected static WebDriver driver;
-//  protected static WebDriverWait wait;
-//
-//  public BasePage(WebDriver driver) {
-//    this.driver = driver;
-//    wait = new WebDriverWait(driver, 10);
-//  }
-//
-//}
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+  WebDriver driver;
+
+
+  public BasePage(WebDriver driver) {
+    this.driver = driver;
+
+  }
+
+}
