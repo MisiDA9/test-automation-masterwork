14cf8070bbe1d3f7e39f90cc1523cfa77fae34e1
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 08:12:17 +0200

    LoginTest was added

---
+++ ReiszMihalyMasterwork/src/test/java/LoginTests.java
@@ -0,0 +1,41 @@
+import Pages.Login;
+import Pages.MyAccount;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+
+
+class LoginTests extends BaseTest {
+  @Test
+  @DisplayName("TC03 - Successful attempt")
+  @Feature("Login")
+  @Description("After a successful login attempt with valid inputs, assert that the \"Sign out\" button is displayed in the header.")
+  public void loginToTheWebshop_Success() throws InterruptedException {
+    Login loginPage = new Login(driver);
+    loginPage.open();
+    loginPage.login("michael@test.com", "pass123");
+
+    MyAccount myAccount = new MyAccount(driver);
+    Assertions.assertTrue(myAccount.signOut().isDisplayed());
+    myAccount.signOut().click();
+  }
+
+  @Test
+  @DisplayName("TC04 - Unsuccessful attempt")
+  @Feature("Login")
+  @Description("After an unsuccessful login attempt with invalid password, assert that the error message is correct.")
+  public void loginToTheWebshop_Failed() throws InterruptedException {
+    Login loginPage = new Login(driver);
+    loginPage.open();
+    loginPage.login("michael@test.com", "Not existing password");
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+    String errorMessage = driver.findElement(By.xpath("//*[@id=\"content\"]/section/div/ul/li")).getText();
+    Assertions.assertEquals(errorMessage,"Authentication failed.");
+  }
+}
