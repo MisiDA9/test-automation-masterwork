57f80267ececb058188e9f149d174d74d3939bac
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 15:41:56 +0200

    DataEntryTest was modified

--- ReiszMihalyMasterwork/src/test/java/DataEntryTest.java
+++ ReiszMihalyMasterwork/src/test/java/DataEntryTest.java
@@ -4,30 +4,22 @@ import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-
-
-import java.util.Random;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 class DataEntryTest extends BaseTest {
   @Test
   @DisplayName("TC08 - Input of new data")
   @Feature("Addresses")
   @Description("After successfully adding a new address, assert that the success message is correct.")
-  public void addNewAddress() throws InterruptedException {
+  public void addNewAddress() {
    signUp();
-   // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-    Thread.sleep(3000);
-    NewAddresses newAddresses = new NewAddresses(driver);
-    newAddresses.open();
-    newAddresses.newAddressesInput("61652 Division Terrace", "Diego", "Texas", "11111");
-    //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-    Thread.sleep(3000);
-    Addresses addresses = new Addresses(driver);
-    //String message = "Address successfully added!"; Nevre jobb megoldas vagy ez az assert is ok?
-    Assertions.assertTrue(addresses.getSuccessfulNewAddressMessage2());
+
+   NewAddresses newAddresses = new NewAddresses(driver);
+   newAddresses.open();
+   newAddresses.newAddressesInput("61652 Division Terrace", "Diego", "Texas", "11111");
+
+   Addresses addresses = new Addresses(driver);
+   wait.until(ExpectedConditions.visibilityOf(addresses.getFeedbackMessageADDDADDRESSSS()));
+   Assertions.assertTrue(addresses.getFeedbackMessageADDDADDRESSSS().getText().equals("Address successfully added!"));
   }
-
-
 }
