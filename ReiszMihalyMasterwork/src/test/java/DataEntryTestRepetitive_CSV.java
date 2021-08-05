2e74e00c2ba489b5f028e5e7c1f9e043666b0bad
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 15:42:04 +0200

    DataEntryTestRepetitive_CSV was modified

--- ReiszMihalyMasterwork/src/test/java/DataEntryTestRepetitive_CSV.java
+++ ReiszMihalyMasterwork/src/test/java/DataEntryTestRepetitive_CSV.java
@@ -9,32 +9,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-
-import java.time.Duration;
-
-
 public class DataEntryTestRepetitive_CSV extends BaseTest {
 
   @BeforeAll
   public void initEach() {
-
-    //loginSTEP
     Login loginPage = new Login(driver);
     loginPage.open();
     loginPage.login("michael@test.com", "pass123");
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
   }
 
   @ParameterizedTest(name = "Data entry: New address has been added with parameterized Test.")
   @CsvFileSource(resources = "/input.csv")
   @Feature("Addresses")
   @Description("After adding an address, the success message should be correct every time.")
-  //@DisplayName("...... ")
   public void paramTest1(String address, String city, String state, String postcode) {
     NewAddresses newAddresses = new NewAddresses(driver);
     newAddresses.open();
     newAddresses.newAddressesInput(address, city, state, postcode);
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
     Addresses addresses = new Addresses(driver);
     Assertions.assertTrue(addresses.getSuccessfulNewAddressMessage2());
