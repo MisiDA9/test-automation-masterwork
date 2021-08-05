993a24caa2f071e7ebd385ae3380edd9c833451f
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 23:18:54 +0200

    DeletingDataTest was modified

--- ReiszMihalyMasterwork/src/test/java/DeletingDataTest.java
+++ ReiszMihalyMasterwork/src/test/java/DeletingDataTest.java
@@ -1,25 +1,29 @@
 import Pages.Addresses;
-import Pages.NewAddresses;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class DeletingDataTest extends BaseTest {
-
   @Test
   @DisplayName("TC_11 - delete an address")
   @Feature("Addresses: deleting address")
   @Description("After deleting an address, the validation message about the successful delete should be correct.")
   public void shouldDeleteAnAddress() {
-      signUp();
-      NewAddresses newAddresses = new NewAddresses(driver);
-      newAddresses.open();
-      newAddresses.newAddressesInput("Roxbury Alley 8", "San Jose", "California", "14136");
-      Addresses addresses = new Addresses(driver);
-      addresses.deleteFirst();
-      Assertions.assertTrue(addresses.getFeedbackMessage().getText().equals("Address successfully deleted!"));
+    signUp();
+    saveANewAddress();
+    deleteFirstAddress();
+
+    Addresses addresses = new Addresses(driver);
+    Assertions.assertTrue(addresses.getFeedbackMessage().getText().equals("Address successfully deleted!"));
+  }
+
+  @Step("Deleting the first address")
+  public void deleteFirstAddress() {
+    Addresses addresses = new Addresses(driver);
+    addresses.deleteFirst();
   }
 }
