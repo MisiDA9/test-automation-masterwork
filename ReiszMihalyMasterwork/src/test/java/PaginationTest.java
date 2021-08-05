68f902c53fdc78f393e74194ece86e38ffc5263a
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 15:43:39 +0200

    Pagination was modified

--- ReiszMihalyMasterwork/src/test/java/PaginationTest.java
+++ ReiszMihalyMasterwork/src/test/java/PaginationTest.java
@@ -7,8 +7,11 @@ import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PaginationTest extends BaseTest {
+
 
 
   @Test
@@ -16,30 +19,32 @@ public class PaginationTest extends BaseTest {
   @Description("After listing all products in the webshop and sorting for ascending price orders and navigating to the next page, every price should be less or equal to the next price on the second page as well")
   @DisplayName("TC_07 Listing products by price and navigating to the next page")
   public void listingData2() throws InterruptedException {
-    Login loginPage = new Login(driver);
-    loginPage.open();
-
-    loginPage.login("michael@test.com", "pass123");
-
-
-    MyAccount myAccount = new MyAccount(driver);
-    myAccount.getLogo().click();
-
-    HomePage homePage = new HomePage(driver);
-    homePage.allTheProdcuts().click();
+//    Login loginPage = new Login(driver);
+//    loginPage.open();
+//
+//    loginPage.login("michael@test.com", "pass123");
+//
+//
+//    MyAccount myAccount = new MyAccount(driver);
+//    myAccount.getLogo().click();
+//
+//    HomePage homePage = new HomePage(driver);
+//    homePage.allTheProdcuts().click();
 
 
     AllTheProducts allTheProducts = new AllTheProducts(driver);
+    allTheProducts.open();
 
     allTheProducts.getFilterSelect().click();
     allTheProducts.getFromLowToHigh().click();
 
-
-    Thread.sleep(1000);
+//    wait.until(ExpectedConditions.elementToBeClickable(allTheProducts.getNextButton()));
+    Thread.sleep(2000);
     allTheProducts.getNextButton().click();
-    Thread.sleep(1000);
-    driver.manage().window().fullscreen();
 
+
+//     wait.until(ExpectedConditions.elementToBeClickable(allTheProducts.getPreviousButton()));
+    Thread.sleep(4000);
     for (int i = 0; i < allTheProducts.getPrices().size()-1; i++) {
       Assertions.assertTrue(allTheProducts.getPrices().get(i) <= allTheProducts.getPrices().get(i + 1));
     }
