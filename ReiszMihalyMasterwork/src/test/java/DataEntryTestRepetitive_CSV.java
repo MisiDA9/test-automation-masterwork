84a9b337203e926d649e8b72b679a52a2bc34614
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 23:18:31 +0200

    DataEntryTestRepetitive_CSV was modified

--- ReiszMihalyMasterwork/src/test/java/DataEntryTestRepetitive_CSV.java
+++ ReiszMihalyMasterwork/src/test/java/DataEntryTestRepetitive_CSV.java
@@ -1,33 +1,35 @@
 import Pages.Addresses;
-import Pages.Login;
 import Pages.NewAddresses;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class DataEntryTestRepetitive_CSV extends BaseTest {
 
+public class DataEntryTestRepetitive_CSV extends BaseTest {
   @BeforeAll
   public void initEach() {
-    Login loginPage = new Login(driver);
-    loginPage.open();
-    loginPage.login("michael@test.com", "pass123");
+    signUp();
   }
 
-  @ParameterizedTest(name = "Data entry: New address has been added with parameterized Test.")
-  @CsvFileSource(resources = "/input.csv")
+  @ParameterizedTest(name = "{4} Data entry: New address has been added with parameterized Test.")
+  @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
   @Feature("Addresses")
   @Description("After adding an address, the success message should be correct every time.")
-  public void paramTest1(String address, String city, String state, String postcode) {
-    NewAddresses newAddresses = new NewAddresses(driver);
-    newAddresses.open();
-    newAddresses.newAddressesInput(address, city, state, postcode);
+  public void paramTest1(String address, String city, String state, String postcode, String testCaseSerialNumber) {
+    saveMultiplyNewAddresses();
 
     Addresses addresses = new Addresses(driver);
-    Assertions.assertTrue(addresses.getSuccessfulNewAddressMessage2());
+    Assertions.assertTrue(addresses.getFeedbackMessageAboutSuccessfulAddressAdding().getText().equals("Address successfully added!"));
+  }
+
+  @Step("Saving multiply new valid addresses from SCV")
+  public void saveMultiplyNewAddresses() {
+    NewAddresses newAddresses = new NewAddresses(driver);
+    newAddresses.open();
+    newAddresses.newAddressesInput("23 Bunting Road", "Naples", "Florida", "11111");
   }
 }
