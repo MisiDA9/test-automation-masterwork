a2ce4360413f76d10d38af7ced355ea280343a99
MisiDA9 <mih.reisz@gmail.com>
2021-08-05 15:42:45 +0200

    LogoutTest was modified

--- ReiszMihalyMasterwork/src/test/java/LogoutTest.java
+++ ReiszMihalyMasterwork/src/test/java/LogoutTest.java
@@ -1,25 +1,20 @@
 import Pages.Login;
 import Pages.MyAccount;
 import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.time.Duration;
 
 class LogoutTest extends BaseTest{
   @Test
   @DisplayName("TC_12 - Successful logout attempt")
   @Feature("Logout")
   @Description("After successful login, log out and assert that the \"Log in to your account\" is displayed")
-
   public void signOut() {
     Login loginPage = new Login(driver);
     loginPage.open();
     loginPage.login("michael@test.hu", "pass123");
-    //.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
     MyAccount myAccount = new MyAccount(driver);
     myAccount.signOut().click();
@@ -27,7 +22,4 @@ class LogoutTest extends BaseTest{
     String exp = loginPage.loginToYourAccount();
     Assertions.assertEquals(exp,"Log in to your account");
   }
-
-
-
 }
