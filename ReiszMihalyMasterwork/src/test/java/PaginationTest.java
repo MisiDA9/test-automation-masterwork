269aaf7f2b1fcf0d5baf2c21ee971021212d297d
MisiDA9 <mih.reisz@gmail.com>
2021-08-23 01:11:03 +0200

    PaginationTest was modified

--- ReiszMihalyMasterwork/src/test/java/PaginationTest.java
+++ ReiszMihalyMasterwork/src/test/java/PaginationTest.java
@@ -5,21 +5,21 @@ import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class PaginationTest extends BaseTest {
   @Test
   @Feature("Pagination")
   @Description("After listing all products in the webshop and sorting for ascending price orders and navigating to the next page, every price should be less or equal to the next price on the second page as well")
   @DisplayName("TC_07 Listing products by price and navigating to the next page")
-  public void listingData2() {
+  public void listingData2() throws InterruptedException {
     AllTheProducts allTheProducts = new AllTheProducts(driver);
     allTheProducts.open();
-
     listingTheProducts();
+    setUpPreconditionForAssertions();
     navigateToTheNextPage();
 
-    wait.until(ExpectedConditions.visibilityOf(allTheProducts.getProductList()));
+    setUpPreconditionForAssertions();
     for (int i = 0; i < allTheProducts.getPrices().size()-1; i++) {
       Assertions.assertTrue(allTheProducts.getPrices().get(i) <= allTheProducts.getPrices().get(i + 1));
     }
@@ -36,5 +36,12 @@ public class PaginationTest extends BaseTest {
   public void navigateToTheNextPage() {
     AllTheProducts allTheProducts = new AllTheProducts(driver);
     allTheProducts.getNextButton().click();
+  }
+
+  @Step ("Set up preconditions for assertion")
+  public void setUpPreconditionForAssertions() throws InterruptedException {
+//    wait.until(ExpectedConditions.visibilityOf(allTheProducts.getProductList()));
+//    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    Thread.sleep(3000);
   }
 }
