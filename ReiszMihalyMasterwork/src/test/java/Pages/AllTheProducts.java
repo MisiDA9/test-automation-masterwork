cb58fca857fa20ea54b054165b742d677e64dfef
MisiDA9 <mih.reisz@gmail.com>
2021-08-23 01:21:08 +0200

    AllTheProducts was modified

--- ReiszMihalyMasterwork/src/test/java/Pages/AllTheProducts.java
+++ ReiszMihalyMasterwork/src/test/java/Pages/AllTheProducts.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import java.util.ArrayList;
 import java.util.List;
-import static java.lang.Integer.parseInt;
 
 public class AllTheProducts {
   WebDriver driver;
@@ -30,22 +29,17 @@ public class AllTheProducts {
     return driver.findElement(By.xpath("//*[@id=\"js-product-list\"]/nav/div[2]/ul/li[3]/a/i"));
   }
 
-  public WebElement getProductList() {
-    return driver.findElement(By.id("js-product-list"));
-  }
-
-  public List<Integer> getPrices() {
+  public List<Float> getPrices() {
     List<WebElement> prices = driver.findElements(By.className("price"));
-    ArrayList<Integer> myList = new ArrayList<Integer>();
-
+    List<String> myList = new ArrayList<>();
     for (int i = 0; i < prices.size(); i++) {
-      if (prices.get(i).getText().length() == 5) {
-        myList.add(i, (parseInt(prices.get(i).getText().substring(1, 2))));
-      } else if (prices.get(i).getText().length() == 6) {
-        myList.add(i, (parseInt(prices.get(i).getText().substring(1, 3))));
-      } else if (prices.get(i).getText().length() == 7)
-        myList.add(i, (parseInt(prices.get(i).getText().substring(1, 4))));
+      String priceString = prices.get(i).getText().substring(1);
+      myList.add(priceString);
     }
-    return myList;
+    List<Float> pricesFloat = new ArrayList<>();
+    for (String stringPrice : myList) {
+      pricesFloat.add(Float.parseFloat(stringPrice));
+    }
+    return pricesFloat;
   }
 }
