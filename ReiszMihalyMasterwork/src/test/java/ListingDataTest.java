adcb1f72c687901b97f4e266c3e0a00e1eb283f0
MisiDA9 <mih.reisz@gmail.com>
2021-09-08 10:18:44 +0200

    ListingDataTest was modified

--- ReiszMihalyMasterwork/src/test/java/ListingDataTest.java
+++ ReiszMihalyMasterwork/src/test/java/ListingDataTest.java
@@ -12,11 +12,10 @@ public class ListingDataTest extends BaseTest {
   @Feature("Listing data")
   @Description("After listing all products in the webshop and sorting for ascending price orders, every price should be less or equal to the next price.")
   @DisplayName("TC_06 Listing products by price - ascending")
-  public void listingData() throws InterruptedException {
+  public void listingData() {
     AllTheProducts allTheProducts = new AllTheProducts(driver);
     allTheProducts.open();
     listingTheProducts();
-    setUpPreconditionForAssertions();
 
     for (int i = 0; i < allTheProducts.getPrices().size()-1; i++) {
       Assertions.assertTrue(allTheProducts.getPrices().get(i) <= allTheProducts.getPrices().get(i + 1));
@@ -30,10 +29,4 @@ public class ListingDataTest extends BaseTest {
     allTheProducts.getFromLowToHigh().click();
   }
 
-  @Step ("Set up preconditions for assertion")
-  public void setUpPreconditionForAssertions() throws InterruptedException {
-//    wait.until(ExpectedConditions.visibilityOf(allTheProducts.getProductList()));
-//    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-    Thread.sleep(2000);
-  }
 }
